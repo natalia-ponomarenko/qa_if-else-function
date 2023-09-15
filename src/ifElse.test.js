a320d888648f38c0da58fbdb390fb90d62e5964f
natalia-ponomarenko <virus.zabutiya@gmail.com>
2023-09-15 19:12:31 +0300

    solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,35 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
 
-  it('should ', () => {
+  it('should return undefined', () => {
+    const condition = jest.fn();
+    const first = jest.fn();
+    const second = jest.fn();
 
+    expect(ifElse(condition, first, second)).toBeUndefined();
   });
 
-  // write tests here
+  it('should execute first callback if condition returns true', () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).toHaveBeenCalled();
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it('should execute second callback if condition returns false', () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn();
+    const second = jest.fn();
+
+    ifElse(condition, first, second);
+
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalled();
+  });
 });
